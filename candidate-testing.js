dacd7d2577d0de38f0e5b58b706381bb50aab3f3
jkr768 <45410359+jkr768@users.noreply.github.com>
2021-06-06 18:34:22 -0500

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -19,7 +19,7 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  for(let i=0;i<questions.length;i++)
+  for(let i=0;i < questions.length;i++)
   {
     
     candidateAnswers.push(input.question(questions[i]));
@@ -32,7 +32,7 @@ function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   let grade;
   let numberOfCorrectAnswer=0;
-  for(let i=0;i<correctAnswers.length;i++)
+  for(let i=0; i < correctAnswers.length; i++)
   {
       if((correctAnswers[i]).toLowerCase()==(candidateAnswers[i]).toLowerCase())
     {
@@ -59,7 +59,7 @@ grade=((numberOfCorrectAnswer)/(questions.length))*100;
   console.log(">>> Status: PASSED! <<<");
 
   return grade;
- }
+}
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
